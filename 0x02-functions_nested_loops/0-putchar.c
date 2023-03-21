e8e9039fc0ed625f184920b50e36eb7a7efd6836
sy1v1a <oyinyechi@ymail.com>
2023-03-21 06:02:28 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,6 @@
 #include "main.h"
 /**
-<<<<<<< HEAD
  * main - print _putchar
-=======
- * main - Prints _putchar
->>>>>>> bc2aa17dd838056443b435f14976a617a3401751
  *
  * Return: 0
  */
@@ -19,9 +15,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-<<<<<<< HEAD
-return (0);
-=======
 return(0);
->>>>>>> bc2aa17dd838056443b435f14976a617a3401751
 }
