0344bcf0739fe1c2c6f6f267eb706132fbd605c2
sy1v1a <oyinyechi@ymail.com>
2023-03-27 04:52:40 -0700

    arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset _to_98 -  a function that resets value to 98
+ *@n: input
+ *return: n
+ */
+void reset_to_98(int *n);
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * int_strlen - returns the length of string
+ * @s: string
+ * return: length
+ */
+int _strlen(char *s);
+{
+int l = 0;
+while (*s[l] != '\0')
+{
+l++;
+}
+return (l);
+}
