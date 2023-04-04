f0e8e05f17a57c9f6cc9f613084ed2165d67cb61
sy1v1a <oyinyechi@ymail.com>
2023-04-04 04:50:51 -0700

    memsetcpy

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * _memset.c - fills memeory with a constant byte
+ * @s: memory to be filled
+ * @b: constant byte
+ * @n: bytes to be changed
+ * return: new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n){
+int d = 0;
+
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ * _memcpy - copies memory area
+ * @dest: where to store memory
+ * @src: where to copy momory
+ * @n: number of bytes
+ * return: n memory bytes and copied memory
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int i = 0;
+
+return (dest);
+}
