afbb7b1f5460638b1f15cadf937db91f8116eab1
sy1v1a <oyinyechi@ymail.com>
2023-05-01 16:19:16 +0100

    list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 /*
- * listint-t _ adds a new node
- * @n: int value
+ * add_nodeint _ adds a new node
+ * @n: new node
+ * @head: first node
  * return: address of new element
  */
 listint_t *add_nodeint(listint_t **head, const int n)
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/*
+ * add_nodeint_end - new node at the end of list
+ * @n: new node
+ * @head: pointer to first nod
+ * return: address of new element or NULL if failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+
