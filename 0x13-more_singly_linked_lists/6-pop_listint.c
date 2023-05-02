c0b7e3a5baf0309c9fb7fe8d7e8e312e742a5cb8
sy1v1a <oyinyechi@ymail.com>
2023-05-01 21:53:35 -0700

    pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * pop_listint - a function to delete head node
+ * @head: pointer to delete
+ * return: 0 if list is empty or the deleted list
+ */
+int pop_listint(listint_t **head)
+{
+	int pop;
+	listint_t del;
+
+	if (!*head || !head)
+		return (0);
+	del = (*head)->next;
+	pop = (*head)->n;
+	*head = del;
+	free(*head);
+
+	return (pop);
+}
