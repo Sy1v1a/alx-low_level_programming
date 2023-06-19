bf5aebe55a95e082327e898948c6f57a9b09dde6
sy1v1a <oyinyechi@ymail.com>
2023-06-19 01:46:18 -0700

    lib

---
+++ 0x18-dynamic_libraries/_puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+void _puts(char *str);
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_puts("\"Keep doing hard things");
+	return (0);
+}
